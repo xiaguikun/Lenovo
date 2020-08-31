4e802389fbea16fc22d2c5c66d2e7faf532fa013
xiaguikun <2973738150@qq.com>
2020-08-31 22:43:55 +0800

    v 1.0.8

--- src/js/index.js
+++ src/js/index.js
@@ -1,42 +1,19 @@
-console.log(1);
+import { mySwiper } from "./swiper.js";
 
+import $ from "./jquery.js";
 
-
-
-
-
-
-
-
-var mySwiper = new Swiper('.swiper-container', {
-    direction: 'horizontal', // 垂直切换选项
-    loop: true, // 循环模式选项
-    autoplay: true, //可选选项，自动滑动
-
-    // 如果需要分页器
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // 如果需要前进后退按钮
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // 如果需要滚动条
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
-    speed: 400,
-    autoplay: {
-        delay: 2500
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-    },
+import "./jquery.lazyload.js"
+$("img.lazy").lazyload({
+    placeholder: "../images/lazy.jpg", //用图片提前占位
+    // placeholder,值为某一图片路径.此图片用来占据将要加载的图片的位置,待图片加载时,占位图则会隐藏
+    effect: "fadeIn", // 载入使用何种效果
+    // effect(特效),值有show(直接显示),fadeIn(淡入),slideDown(下拉)等,常用fadeIn
+    // threshold: 200, // 提前开始加载
+    // threshold,值为数字,代表页面高度.如设置为200,表示滚动条在离目标位置还有200的高度时就开始加载图片,可以做到不让用户察觉
+    // event: 'click',  // 事件触发时才加载
+    // event,值有click(点击),mouseover(鼠标划过),sporty(运动的),foobar(…).可以实现鼠标莫过或点击图片才开始加载,后两个值未测试…
+    // container: $("#container"),  // 对某容器中的图片实现效果
+    // container,值为某容器.lazyload默认在拉动浏览器滚动条时生效,这个参数可以让你在拉动某DIV的滚动条时依次加载其中的图片
+    // failurelimit : 10 // 图片排序混乱时
+    // failurelimit,值为数字.lazyload默认在找到第一张不在可见区域里的图片时则不再继续加载,但当HTML容器混乱的时候可能出现可见区域内图片并没加载出来的情况,failurelimit意在加载N张可见区域外的图片,以避免出现这个问题.
 });
---
+++ src/js/swiper.js
@@ -0,0 +1,35 @@
+import Swiper from './swiper-bundle.js';
+
+var mySwiper = new Swiper('.swiper-container', {
+    direction: 'horizontal', // 垂直切换选项
+    loop: true, // 循环模式选项
+    autoplay: true, //可选选项，自动滑动
+
+    // 如果需要分页器
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // 如果需要前进后退按钮
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // 如果需要滚动条
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+    speed: 400,
+    autoplay: {
+        delay: 2500
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+export { mySwiper };
